dc622cf2797dd8c0873823de96debacd84a260d7
若相忆 <liuswi@qq.com>
2015-07-29 18:49:25 +0800

    lsw

---
+++ VideoProcessing/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoProcessing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExcuteProcess("../../ffmpeg.exe", "-y -i D:\\1.flv -ss 00:00:15 D:\\1.jpg", (s, e) => Console.WriteLine(e.Data));
+        }
+
+        static void ExcuteProcess(string exe, string arg, DataReceivedEventHandler output) 
+        {
+            using (var p = new Process())
+            {
+                p.StartInfo.FileName = exe;
+                p.StartInfo.Arguments = arg;
+
+                p.StartInfo.UseShellExecute = false;
+                p.StartInfo.CreateNoWindow = true;
+                p.StartInfo.RedirectStandardError = true;
+                p.StartInfo.RedirectStandardOutput = true;
+
+                p.OutputDataReceived += output;
+                p.ErrorDataReceived += output;
+
+                p.Start();
+                p.BeginOutputReadLine();
+                p.BeginErrorReadLine();
+                p.WaitForExit();
+            }
+        }
+    }
+}
